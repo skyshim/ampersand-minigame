9b996bb8644221f89a3d52db0df57a099039f7bc
JSD0827 <jsdhan0827@gmail.com>
2025-07-30 20:07:35 +0900

    1753873660_JSDHan

---
+++ Assets/Scenes/Mine/Scripts/MineGameHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineGameHandler : MonoBehaviour
+{
+    private Map map;
+
+    private void Start(){
+        map = new Map();
+    }
+}
