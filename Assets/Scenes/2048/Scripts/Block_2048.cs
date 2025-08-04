751450280a0c23e05826d5fba7d0e68db91702b7
0youzzan <0kong5131@gmail.com>
2025-08-04 22:06:12 +0900

    2025.08.04 - 2048(4)
    병합함수제작 >> 추후 블록 삭제 기능 추가 필요
    블록 이동 함수 제작 >> 추후 블록 이동 지시 함수 필요

--- Assets/Scenes/2048/Scripts/Block_2048.cs
+++ Assets/Scenes/2048/Scripts/Block_2048.cs
@@ -6,22 +6,75 @@ public class Block_2048 : MonoBehaviour
 {
     public Player_2048 pCode; // 플레이어 코드
 
+    private int idx; // 블록 위치 코드 인덱스
+    private int myValue; // 블록 값
+
+    float MoveTime = 0.1f; // 이동 시간 (초 단위)
+    float speed = 0f; // 이동 속도
+
+    bool moveY = false; // Y축 이동 여부
+    bool moveX = false; // X축 이동 여부
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < pCode.posCode.Length; i++) { // 블록 위치 코드 리스트에서
+            if (pCode.posCode[i] == transform.position) { // 이동 시작 위치와 일치하는지 확인
+                idx = i; // 인덱스 저장
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pCode.isMoving[idx] == true) { // 이동 중이라면
+            if (moveY == true) { // Y축 이동이면
+                MoveY(pCode.posCode[idx]); // Y축 이동 함수 호출
+            }
+            else if (moveX == true) { // X축 이동이면
+                MoveX(pCode.posCode[idx]); // X축 이동 함수 호출
+            }
+        }
     }
 
 
-    // 이동 함수
-    public void Move(Vector3 from, int distance) 
+    // 이동 속도 설정 함수
+    public void Move(Vector3 from, Vector3 to, string direction) 
     {
-        
+        if (pCode.isMoving[idx] == true) { // 이동 중이라면 리턴
+            return;
+        }
+        else {
+            pCode.isMoving[idx] = true;
+        }
+
+        if (direction == "Y") {
+            speed = to.y - from.y;
+            moveY = true;
+        }
+        else if (direction == "X") {
+            speed = to.x - from.x;
+            moveX = true;
+        }
+    }
+
+    // Y축 이동 함수
+    void MoveY(Vector3 destination) 
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (transform.position == destination) {
+            pCode.isMoving[idx] = false; // 이동 완료 후 이동 중 상태 해제
+        }
+    }
+    // X축 이동 함수
+    void MoveX(Vector3 destination) 
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (transform.position == destination) {
+            pCode.isMoving[idx] = false; // 이동 완료 후 이동 중 상태 해제
+        }
     }
 }
--- Assets/Scenes/2048/Scripts/Player_2048.cs
+++ Assets/Scenes/2048/Scripts/Player_2048.cs
@@ -18,10 +18,12 @@ public class Player_2048 : MonoBehaviour
     public GameObject block2048;
 
 
-    public Vector3[] posCode = new Vector3[16]; // 블록 위치 코드 리스트
+    public Vector3[] posCode = new Vector3[16]; // 블록 위치 코드화
     public float[] x = { -1.8f, -0.6f, 0.6f, 1.8f }; // x위치코드
     public float[] y = { 0.3f, -0.9f, -2.1f, -3.3f }; // y위치코드
     public int[] blockValue = new int[16]; // 블록 값 리스트
+    public bool[] isMoving = new bool[16]; // 블록 이동 중인지 여부 리스트
+    public int[,] arr_des = new int[16,2]; // 블록 이동 위치 배열
 
     public bool playing = false;
     // Start is called before the first frame update
@@ -40,6 +42,12 @@ public class Player_2048 : MonoBehaviour
         for (int i = 0; i < 16; i++) {
             blockValue[i] = 0; // 블록 값 초기화
         }
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr_des[i,j] = 0; // 블록 이동 위치 배열 초기화
+            }
+        }
+
 
 
     }
@@ -52,13 +60,25 @@ public class Player_2048 : MonoBehaviour
             GameStart();
         }
 
+        string direction = ""; // 이동 방향 변수 초기화
+
         // 입력받기
         bool Linput = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow); 
         bool Rinput = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
         bool Uinput = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow); 
         bool Dinput = Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
+        
+        if (Linput) { // 왼쪽 이동
+            direction = "A"; // 왼쪽 이동 방향 설정
+        } else if (Rinput) { // 오른쪽 이동
+            direction = "D"; // 오른쪽 이동 방향 설정
+        } else if (Uinput) { // 위쪽 이동
+            direction = "W"; // 위쪽 이동 방향 설정
+        } else if (Dinput) { // 아래쪽 이동
+            direction = "S"; // 아래쪽 이동 방향 설정
+        }
 
-
+        MergeProcess(direction); // 블록 이동 전 병합 처리
 
     }
 
@@ -99,6 +119,76 @@ public class Player_2048 : MonoBehaviour
         } else { // 4블록 생성
             Instantiate(block4, spawnPos, Quaternion.identity);
             blockValue[spawnPosCode] = 4; // 블록 값 설정
+        }
+    }
+
+
+    public void CreateMergedBlock(Vector3 spawnPos, int spawnNum) {
+        if (spawnNum == 4) {
+            Instantiate(block4, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 8) {
+            Instantiate(block8, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 16) {
+            Instantiate(block16, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 32) {
+            Instantiate(block32, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 64) {
+            Instantiate(block64, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 128) {
+            Instantiate(block128, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 256) {
+            Instantiate(block256, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 512) {
+            Instantiate(block512, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 1024) {
+            Instantiate(block1024, spawnPos, Quaternion.identity);
+        } else if (spawnNum == 2048) {
+            Instantiate(block2048, spawnPos, Quaternion.identity);
+        }
+    }
+
+
+    // 블록 이동 전 병합 처리 함수
+    public void MergeProcess(string direction) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (direction == "A") { // 왼쪽 이동 시 병합
+                    if (blockValue[i * 4 + j] == blockValue[i * 4 + j + 1] && blockValue[i * 4 + j] != 0) {
+                        blockValue[i * 4 + j + 1] *= 2; // 병합된 블록 값 설정
+                        CreateMergedBlock(posCode[i * 4 + j], blockValue[i * 4 + j]); // 병합된 블록 생성
+                        blockValue[i * 4 + j + 1] = 0; // 병합된 블록 제거
+                    }
+                }
+                else if (direction == "D") { // 오른쪽 이동 시 병합
+                    if (blockValue[i * 4 + 4 - j] == blockValue[i * 4 + 3 - j] && blockValue[i * 4 + 4 - j] != 0) {
+                        blockValue[(i + 1) * 4 + 4 - j] *= 2;
+                        CreateMergedBlock(posCode[i * 4 + 4 - j], blockValue[i * 4 + 4 - j]);
+                        blockValue[(i + 1) * 4 + 3 - j] = 0;
+                    }
+                }
+                else if (direction == "W") { // 위쪽 이동 시 병합
+                    if (blockValue[(4 - i) * 4 + j] == blockValue[(3 - i) * 4 + j] && blockValue[i * 4 + j] != 0) {
+                        blockValue[(4 - i) * 4 + j] *= 2;
+                        CreateMergedBlock(posCode[(4 - i) * 4 + j], blockValue[(4 - i) * 4 + j]);
+                        blockValue[(3 - i) * 4 + j] = 0;
+                    }
+                }
+                else if (direction == "S") { // 아래쪽 이동 시 병합
+                    if (blockValue[i * 4 + j] == blockValue[(i + 1) * 4 + j] && blockValue[i * 4 + j] != 0) {
+                        blockValue[i * 4 + j] *= 2;
+                        CreateMergedBlock(posCode[i * 4 + j], blockValue[i * 4 + j]);
+                        blockValue[(i + 1) * 4 + j] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+
+    // 블록 이동 명령 함수
+    public void MoveCommand(string direction) {
+        if (direction == "D") {
+
         }
     }
 }
