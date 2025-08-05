6be832eb28dafc95ec5eb8efc96bfa440a72cf0a
ahnjb <159648234+jeongbin-0509@users.noreply.github.com>
2025-08-05 16:56:15 +0900

    2025.08.05 - Money(2)

--- Assets/Scenes/Money/Scripts/MoneyManager.cs
+++ Assets/Scenes/Money/Scripts/MoneyManager.cs
@@ -1,28 +1,28 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using TMPro;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
-//public class Money : MonoBehaviour
-//{
-//    public TextMeshProUGUI Moneytext;
+public class Money : MonoBehaviour
+{
+    public TextMeshProUGUI Moneytext;
 
-//    private float MoneyNum = 0f;
-//    //private float MoneyValue = 1f;
+    private float MoneyNum = 0f;
+    private float MoneyValue = 1f;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
+    //Start is called before the first frame update
+    void Start()
+    {
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if(Input.GetMouseButtonDown(0))
-//        {
-//            MoneyNum += MoneyValue;
-//            Moneytext.text = "Money: " + MoneyNum;
-//        }
-//    }
-//}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoneyNum += MoneyValue;
+            Moneytext.text = "Money: " + MoneyNum;
+        }
+    }
+}
