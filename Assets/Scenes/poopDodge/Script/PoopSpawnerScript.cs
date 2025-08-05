e926589cc4438d1acde027fa5d090426ed5a03bc
신범서 <sapltv090410@gmail.com>
2025-08-05 21:00:47 +0900

    똥 피하기 완?성
    다시 시작 기능
    종료 기능
    1인 2인 선택 가능
    (대쉬 만들까 고민중)

--- Assets/Scenes/poopDodge/Script/PoopSpawnerScript.cs
+++ Assets/Scenes/poopDodge/Script/PoopSpawnerScript.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class PoopSpawnerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject poops;
     public GameObject manager;
+    public Slider controller;
 
     public float time = 1f;
     public float afterSpawn = 0f;
@@ -19,6 +22,7 @@ public class PoopSpawnerScript : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         spawnRate = Random.Range(0.5f, 1f);
+        controller.onValueChanged.AddListener(move);
     }
 
     // Update is called once per frame
@@ -32,11 +36,30 @@ public class PoopSpawnerScript : MonoBehaviour
             if (afterSpawn > spawnRate)
             {
                 afterSpawn = 0f;
-                transform.position = new Vector3(Random.Range(-4, 4), 8, 0);
+                transform.position = new Vector3(Random.Range(-4, 5), 8, 0);
                 GameObject poop = Instantiate(poops, transform.position, transform.rotation);
 
-                spawnRate = Random.Range(0f, 0.7f);
+                spawnRate = Random.Range(0.3f, 0.7f);
             }
         }
+        else if (managerScript.isGameStart && !managerScript.isGameOver && managerScript.gamemode == 2)
+        {
+            controller.gameObject.SetActive(true);
+            rb.position = new Vector2(rb.position.x, 5);
+            
+            afterSpawn += Time.deltaTime;
+            if (afterSpawn > spawnRate)
+            {
+                afterSpawn = 0f;
+                GameObject poop = Instantiate(poops, transform.position, transform.rotation);
+
+                spawnRate = Random.Range(0.5f, 1.5f);
+            }
+        }
+    }
+    
+    void move(float value)
+    {
+        rb.position = new Vector2(value, 5);
     }
 }
