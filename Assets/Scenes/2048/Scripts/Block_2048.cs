72b7270f7075a079214746550071328510093305
0youzzan <0kong5131@gmail.com>
2025-08-05 22:03:19 +0900

    2025.08.05 - 2048(5)
    시팔 모르겠다

--- Assets/Scenes/2048/Scripts/Block_2048.cs
+++ Assets/Scenes/2048/Scripts/Block_2048.cs
@@ -2,79 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block_2048 : MonoBehaviour
-{
+public class Block_2048 : MonoBehaviour {
     public Player_2048 pCode; // 플레이어 코드
 
-    private int idx; // 블록 위치 코드 인덱스
+    private int Xidx; // 블록 위치 코드 인덱스
+    private int Yidx;
     private int myValue; // 블록 값
 
-    float MoveTime = 0.1f; // 이동 시간 (초 단위)
-    float speed = 0f; // 이동 속도
+    private float speed = 0f; // 이동 속도
 
     bool moveY = false; // Y축 이동 여부
     bool moveX = false; // X축 이동 여부
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < pCode.posCode.Length; i++) { // 블록 위치 코드 리스트에서
-            if (pCode.posCode[i] == transform.position) { // 이동 시작 위치와 일치하는지 확인
-                idx = i; // 인덱스 저장
-                break;
+    void Start() {
+        if (pCode == null) {
+            pCode = FindObjectOfType<Player_2048>();
+        }
+
+        for (int i = 0; i < 4; i++) { // 블록 위치 코드 리스트에서
+            for (int j = 0; j < 4; j++) {
+                if (Vector3.Distance(pCode.posCode[i*4+j],transform.position) < 0.01f) { // 이동 시작 위치와 일치하는지 확인
+                    Xidx = j;
+                    Yidx = i; // 인덱스 저장
+                    break;
+                }
             }
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (pCode.isMoving[idx] == true) { // 이동 중이라면
+    void Update() {
+        if (pCode.commanding == true) {
+            Move(pCode.posCode[pCode.arr_des[Xidx,Yidx, 0]], pCode.posCode[pCode.arr_des[Xidx,Yidx, 1]], pCode.moveDirection); // 이동 함수 호출
+        }
+
+        if (pCode.isMoving[Xidx,Yidx] == true) { // 이동 중이라면
             if (moveY == true) { // Y축 이동이면
-                MoveY(pCode.posCode[idx]); // Y축 이동 함수 호출
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, pCode.posCode[pCode.arr_des[Xidx,Yidx, 1]]) < 0.01f) {
+                    pCode.isMoving[Xidx,Yidx] = false; // 이동 완료 후 이동 중 상태 해제
+                }
             }
             else if (moveX == true) { // X축 이동이면
-                MoveX(pCode.posCode[idx]); // X축 이동 함수 호출
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, pCode.posCode[pCode.arr_des[Xidx, Yidx, 1]]) < 0.01f) {
+                    pCode.isMoving[Xidx, Yidx] = false; // 이동 완료 후 이동 중 상태 해제
+                }
             }
         }
     }
 
 
     // 이동 속도 설정 함수
-    public void Move(Vector3 from, Vector3 to, string direction) 
-    {
-        if (pCode.isMoving[idx] == true) { // 이동 중이라면 리턴
+    public void Move(Vector3 from, Vector3 to, string direction) {
+        if (pCode.isMoving[Xidx,Yidx] == true) { // 이동 중이라면 리턴
             return;
         }
         else {
-            pCode.isMoving[idx] = true;
+            pCode.isMoving[Xidx,Yidx] = true;
         }
 
-        if (direction == "Y") {
+        if (direction == "W" || direction == "S") {
             speed = to.y - from.y;
             moveY = true;
         }
-        else if (direction == "X") {
+        else if (direction == "A" || direction == "D") {
             speed = to.x - from.x;
             moveX = true;
         }
     }
 
-    // Y축 이동 함수
-    void MoveY(Vector3 destination) 
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        if (transform.position == destination) {
-            pCode.isMoving[idx] = false; // 이동 완료 후 이동 중 상태 해제
-        }
-    }
-    // X축 이동 함수
-    void MoveX(Vector3 destination) 
-    {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
-        if (transform.position == destination) {
-            pCode.isMoving[idx] = false; // 이동 완료 후 이동 중 상태 해제
-        }
+    public void Die() {
+        Destroy(gameObject); // 블록 제거
     }
 }
