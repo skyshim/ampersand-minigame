3022bd16ed059fd8899b7d647dde70d4693c981c
skyshim <skyshim0128@gmail.com>
2025-08-06 22:41:21 +0900

    2025.08.06 - 파이어베이스 처리
    아오 진짜 화딱지나죽겠네

--- Assets/FirebaseInit.cs
+++ Assets/FirebaseInit.cs
@@ -1,26 +1,26 @@
-using Firebase;
-using Firebase.Auth;
-using UnityEngine;
+//using Firebase;
+//using Firebase.Auth;
+//using UnityEngine;
 
-public class FirebaseInit : MonoBehaviour
-{
-    public static FirebaseAuth auth;
-    public static FirebaseUser user;
+//public class FirebaseInit : MonoBehaviour
+//{
+//    public static FirebaseAuth auth;
+//    public static FirebaseUser user;
 
-    void Start()
-    {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
-        {
-            var status = task.Result;
-            if (status == DependencyStatus.Available)
-            {
-                auth = FirebaseAuth.DefaultInstance;
-                Debug.Log("Firebase Auth initialized!");
-            }
-            else
-            {
-                Debug.LogError("Firebase dependency error: " + status);
-            }
-        });
-    }
-}
+//    void Start()
+//    {
+//        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+//        {
+//            var status = task.Result;
+//            if (status == DependencyStatus.Available)
+//            {
+//                auth = FirebaseAuth.DefaultInstance;
+//                Debug.Log("Firebase Auth initialized!");
+//            }
+//            else
+//            {
+//                Debug.LogError("Firebase dependency error: " + status);
+//            }
+//        });
+//    }
+//}
