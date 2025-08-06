ff5d0c3cf5897a04ed209b1935106ce37eb7a434
0youzzan <0kong5131@gmail.com>
2025-08-06 21:37:40 +0900

    2025.08.06 - 2048(6)
    시발 더러운 새끼

--- Assets/Scenes/2048/Scripts/Block_2048.cs
+++ Assets/Scenes/2048/Scripts/Block_2048.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class Block_2048 : MonoBehaviour {
     public Player_2048 pCode; // 플레이어 코드
 
-    private int Xidx; // 블록 위치 코드 인덱스
-    private int Yidx;
-    private int myValue; // 블록 값
-
-    private float speed = 0f; // 이동 속도
-
-    bool moveY = false; // Y축 이동 여부
-    bool moveX = false; // X축 이동 여부
-
 
     // Start is called before the first frame update
     void Start() {
@@ -21,60 +12,16 @@ public class Block_2048 : MonoBehaviour {
             pCode = FindObjectOfType<Player_2048>();
         }
 
-        for (int i = 0; i < 4; i++) { // 블록 위치 코드 리스트에서
-            for (int j = 0; j < 4; j++) {
-                if (Vector3.Distance(pCode.posCode[i*4+j],transform.position) < 0.01f) { // 이동 시작 위치와 일치하는지 확인
-                    Xidx = j;
-                    Yidx = i; // 인덱스 저장
-                    break;
-                }
-            }
-        }
     }
 
-    // Update is called once per frame
-    void Update() {
-        if (pCode.commanding == true) {
-            Move(pCode.posCode[pCode.arr_des[Xidx,Yidx, 0]], pCode.posCode[pCode.arr_des[Xidx,Yidx, 1]], pCode.moveDirection); // 이동 함수 호출
-        }
-
-        if (pCode.isMoving[Xidx,Yidx] == true) { // 이동 중이라면
-            if (moveY == true) { // Y축 이동이면
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                if (Vector3.Distance(transform.position, pCode.posCode[pCode.arr_des[Xidx,Yidx, 1]]) < 0.01f) {
-                    pCode.isMoving[Xidx,Yidx] = false; // 이동 완료 후 이동 중 상태 해제
-                }
-            }
-            else if (moveX == true) { // X축 이동이면
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-                if (Vector3.Distance(transform.position, pCode.posCode[pCode.arr_des[Xidx, Yidx, 1]]) < 0.01f) {
-                    pCode.isMoving[Xidx, Yidx] = false; // 이동 완료 후 이동 중 상태 해제
-                }
-            }
-        }
-    }
-
-
-    // 이동 속도 설정 함수
-    public void Move(Vector3 from, Vector3 to, string direction) {
-        if (pCode.isMoving[Xidx,Yidx] == true) { // 이동 중이라면 리턴
-            return;
-        }
-        else {
-            pCode.isMoving[Xidx,Yidx] = true;
-        }
-
-        if (direction == "W" || direction == "S") {
-            speed = to.y - from.y;
-            moveY = true;
-        }
-        else if (direction == "A" || direction == "D") {
-            speed = to.x - from.x;
-            moveX = true;
-        }
-    }
-
+    // 블록 제거 함수
     public void Die() {
-        Destroy(gameObject); // 블록 제거
+        Destroy(gameObject);
+    }
+
+
+    // 이동 함수
+    public void Move(Vector2 from, Vector2 to) {
+
     }
 }
--- Assets/Scenes/2048/Scripts/Player_2048.cs
+++ Assets/Scenes/2048/Scripts/Player_2048.cs
@@ -16,230 +16,110 @@ public class Player_2048 : MonoBehaviour {
     public GameObject block1024;
     public GameObject block2048;
 
-    public GameObject[,] blockMap = new GameObject[4, 4];
+    public GameObject[,] blockMap = new GameObject[4, 4]; // 해당 위치에 존재하는 블록 프리펩 저장
+    public int[,] blockValue = new int[4, 4]; // 해당 위치에 존재하는 블록 값 저장
 
-    public Vector3[] posCode = new Vector3[16]; // 블록 위치 코드화
+
+    public Vector2[,] posCode = new Vector2[4,4]; // 블록 위치 코드화
     public float[] x = { -1.8f, -0.6f, 0.6f, 1.8f }; // x위치코드
     public float[] y = { 0.3f, -0.9f, -2.1f, -3.3f }; // y위치코드
-    public int[,] blockValue = new int[4, 4]; // 블록 값 리스트
-    public bool[,] isMoving = new bool[4, 4]; // 블록 이동 중인지 여부 리스트
-    public int[,,] arr_des = new int[4, 4, 2]; // 블록 이동 위치 배열
-    public string moveDirection = "";
-
-    public bool playing = false;
-    public bool commanding = false;
-    public bool merged = false;
 
 
-    // Start is called before the first frame update
-    void Start() {
-        // 블록 위치 코드 설정 및 리스트 초기화
-        int idx = 0;
+    private void Start() {
+        // 블록 위치 코드 초기화
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                posCode[idx] = new Vector3(x[i], y[j], -2f); // 블록 위치 코드 설정
+                posCode[i,j] = new Vector2(x[i], y[j]);
                 blockValue[i, j] = 0; // 블록 값 초기화
-                isMoving[i, j] = false; // 블록 이동 중 여부 초기화
-                for (int k = 0; k < 2; k++) {
-                    arr_des[i, j, k] = 0; // 블록 이동 위치 배열 초기화
-                }
-                idx++;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update() {
-
-        if (Input.anyKeyDown && (playing == false)) { // 게임 시작   
-            GameStart();
-        }
-
-        // 입력받기
-        bool Linput = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
-        bool Rinput = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
-        bool Uinput = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
-        bool Dinput = Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
-
-        if (Linput) { // 왼쪽 이동
-            moveDirection = "A"; // 왼쪽 이동 방향 설정
-            commanding = true; // 명령 중 상태 설정
-            merged = false;
-        }
-        else if (Rinput) { // 오른쪽 이동
-            moveDirection = "D"; // 오른쪽 이동 방향 설정
-            commanding = true;
-            merged = false; // 병합 여부 초기화
-        }
-        else if (Uinput) { // 위쪽 이동
-            moveDirection = "W"; // 위쪽 이동 방향 설정
-            commanding = true; // 명령 중 상태 설정
-            merged = false;
-        }
-        else if (Dinput) { // 아래쪽 이동
-            moveDirection = "S"; // 아래쪽 이동 방향 설정
-            commanding = true; // 명령 중 상태 설정
-            merged = false;
-        }
-
-        if (commanding == false) return; // 명령 중이 아니면 리턴
-        if (merged == false) MergeProcess(moveDirection); // 블록 이동 전 병합 처리
-        MoveCommand(moveDirection); // 블록 이동 명령 처리
-        CreateBlock(); // 블록 생성
-    }
-
-
-    // 게임 시작
-    void GameStart() {
-        CreateBlock();
-        CreateBlock(); // 블록 2개 생성
-        playing = true;
+    private void Update() {
 
     }
 
-
-    // 블록 생성 함수
     public void CreateBlock() {
-        int XspawnPosCode; // 블록 생성 위치 코드
-        int YspawnPosCode;
+        // 2 또는 4 블록 생성
+        int blockType = Random.Range(0, 10);
+        GameObject blockPrefab = null;
+        if (blockType < 9) {
+            blockPrefab = block2; // 90% 확률로 2 블록 생성
+            blockType = 2; // 블록 값 2로 설정
+        } else {
+            blockPrefab = block4; // 10% 확률로 4 블록 생성
+            blockType = 4; // 블록 값 4로 설정
+        }
 
-        // 블록 생성 위치 탐색
-        int idx = 0;
-        while (true) {
-            XspawnPosCode = Random.Range(0, 4); // 0~3 사이의 랜덤 코드 생성
-            YspawnPosCode = Random.Range(0, 4);
-
-            if (blockValue[XspawnPosCode, YspawnPosCode] == 0) { // 블록이 없는 위치를 찾음
-                break; // 블록 생성 위치 탐색 종료
+        // 빈 칸에 블록 생성
+        do {
+            int i = Random.Range(0, 4);
+            int j = Random.Range(0, 4);
+            if (blockMap[i, j] == null) { // 빈 칸에만 생성
+                GameObject newBlock = Instantiate(blockPrefab, new Vector2(i, j), Quaternion.identity);
+                blockMap[i, j] = newBlock;
+                blockValue[i, j] = blockType; // 블록 값 설정
+                break; // 블록을 생성한 후 반복문 종료
             }
-            if (idx > 17) { // 블록 생성 위치 탐색 실패
-                Debug.Log("블록 생성 위치 탐색 실패");
-                return; // 블록 생성 위치 탐색 실패시 함수 종료
-            }
-            idx++;
-        }
-        Vector3 spawnPos = posCode[YspawnPosCode * 4 + XspawnPosCode];
-
-        // 블록 생성
-        int spawnWhat = Random.Range(0, 2);
-        if (spawnWhat == 0) { // 2블록 생성
-            blockMap[XspawnPosCode, YspawnPosCode] = Instantiate(block2, spawnPos, Quaternion.identity);
-            blockValue[XspawnPosCode, YspawnPosCode] = 2; // 블록 값 설정
-        }
-        else { // 4블록 생성
-            blockMap[XspawnPosCode, YspawnPosCode] = Instantiate(block4, spawnPos, Quaternion.identity);
-            blockValue[XspawnPosCode, YspawnPosCode] = 4; // 블록 값 설정
-        }
-
-        if (commanding == true) { // 명령 중 상태라면
-            commanding = false; // 명령 중 상태 해제
-        }
+        } while (true);
     }
 
 
-    public void CreateMergedBlock(Vector3 spawnPos, int spawnNum) {
-        if (spawnNum == 4) {
-            Instantiate(block4, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 8) {
-            Instantiate(block8, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 16) {
-            Instantiate(block16, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 32) {
-            Instantiate(block32, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 64) {
-            Instantiate(block64, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 128) {
-            Instantiate(block128, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 256) {
-            Instantiate(block256, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 512) {
-            Instantiate(block512, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 1024) {
-            Instantiate(block1024, spawnPos, Quaternion.identity);
-        }
-        else if (spawnNum == 2048) {
-            Instantiate(block2048, spawnPos, Quaternion.identity);
-        }
-    }
-
-
-    // 블록 이동 전 병합 처리 함수
-    public void MergeProcess(string direction) {
-        if (direction == "A") { // 왼쪽 이동 시 병합
-            for (int i = 0; i < 4; i++) { // 각 행에 대해
-                for (int j = 0; j < 3; j++) { // 각 열에 대해
-                    if (blockValue[j, i] == blockValue[j + 1, i] && blockValue[j, i] != 0) {
-                        blockMap[j, i].GetComponent<Block_2048>().Die();
-                        blockMap[j + 1, i].GetComponent<Block_2048>().Die(); // 병합할 블록 제거
-
-                        blockValue[i, j] *= 2; // 병합된 블록 값 설정
-                        CreateMergedBlock(posCode[i * 4 + j], blockValue[j,i]); // 병합된 블록 생성
-                        blockValue[j+1,i] = 0; // 병합된 블록 제거
+    private void CommandLeft() {
+        bool isMoving = true;
+        for (int y = 0; y < 4; y++) {
+            for (int x = 1; x < 4; x++) {
+                if (blockMap[x, y] != null) {
+                    int targetX = x;
+                    while (targetX > 0 && blockMap[targetX - 1, y] == null) {
+                        targetX--;
                     }
-                }
-            }
-        }
-        else if (direction == "D") { // 오른쪽 이동 시 병합
-            for (int i = 0; i < 4; i++) { // 각 행에 대해
-                for (int j = 3; j > 0; j--) { // 각 열에 대해
-                    if (blockValue[j,i] == blockValue[j-1,i] && blockValue[j,i] != 0) {
-                        blockMap[j, i].GetComponent<Block_2048>().Die();
-                        blockMap[j - 1, i].GetComponent<Block_2048>().Die(); // 병합할 블록 제거
 
-                        blockValue[j,i] *= 2; // 병합된 블록 값 설정
-                        CreateMergedBlock(posCode[i * 4 + j], blockValue[j,i]); // 병합된 블록 생성
-                        blockValue[j-1,i] = 0; // 병합된 블록 제거
-                    }
-                }
-            }
-        }
-        else if (direction == "W") { // 위쪽 이동 시 병합
-            for (int j = 0; j < 4; j++) { // 각 열에 대해
-                for (int i = 0; i < 3; i++) { // 각 행에 대해
-                    if (blockValue[j,i] == blockValue[j,i+1] && blockValue[j,i] != 0) {
-                        blockMap[j, i].GetComponent<Block_2048>().Die();
-                        blockMap[j, i + 1].GetComponent<Block_2048>().Die(); // 병합할 블록 제거
+                    if (targetX != x) {
+                        blockMap[targetX, y] = blockMap[x, y];
+                        blockValue[targetX, y] = blockValue[x, y];
 
-                        blockValue[j,i] *= 2; // 병합된 블록 값 설정
-                        CreateMergedBlock(posCode[i * 4 + j], blockValue[j,i]); // 병합된 블록 생성
-                        blockValue[j,i+1] = 0; // 병합된 블록 제거
-                    }
-                }
-            }
-        }
-        else if (direction == "S") { // 아래쪽 이동 시 병합
-            for (int j = 0; j < 4; j++) { // 각 열에 대해
-                for (int i = 3; i > 0; i--) { // 각 행에 대해
-                    if (blockValue[j,i] == blockValue[j,i-1] && blockValue[j,i] != 0) {
-                        blockMap[j, i].GetComponent<Block_2048>().Die();
-                        blockMap[j, i - 1].GetComponent<Block_2048>().Die(); // 병합할 블록 제거
+                        blockMap[x, y] = null;
+                        blockValue[x, y] = 0;
 
-                        blockValue[j,i] *= 2; // 병합된 블록 값 설정
-                        CreateMergedBlock(posCode[i * 4 + j], blockValue[j,i]); // 병합된 블록 생성
-                        blockValue[j,i-1] = 0; // 병합된 블록 제거
+                        blockMap[targetX, y].GetComponent<Block_2048>().Move(posCode[x, y], posCode[targetX, y]);
                     }
                 }
             }
         }
 
-        merged = true; // 병합 완료 상태 설정
-    }
+        if (isMoving) return;
+        //합치기 단계
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (blockValue[i, j] == blockValue[i, j + 1] && blockValue[i, j] != 0) { // 블록과 왼쪽 블록 값 비교
+                    // 블록 제거
+                    blockMap[i, j].GetComponent<Block_2048>().Die();
+                    blockMap[i, j + 1].GetComponent<Block_2048>().Die();
 
+                    // 변수 업데이트
+                    blockMap[i, j] = null; blockMap[i, j + 1] = null; // 현재 블록 제거
+                    blockValue[i, j] *= 2; blockValue[i, j + 1] = 0; // 블록 값 업데이트
 
-    // 블록 이동 명령 함수
-    public void MoveCommand(string direction) {
-        if (direction == "A") {
-            for (int y = 0; y < 4; y++) {
-
+                    // 병합된 블록 생성
+                    GameObject blockPrefab = null;
+                    switch (blockValue[i, j]) {
+                        case 4: blockPrefab = block4; break;
+                        case 8: blockPrefab = block8; break;
+                        case 16: blockPrefab = block16; break;
+                        case 32: blockPrefab = block32; break;
+                        case 64: blockPrefab = block64; break;
+                        case 128: blockPrefab = block128; break;
+                        case 256: blockPrefab = block256; break;
+                        case 512: blockPrefab = block512; break;
+                        case 1024: blockPrefab = block1024; break;
+                        case 2048: blockPrefab = block2048; break;
+                    }
+                    GameObject newBlock = Instantiate(blockPrefab, new Vector2(i, j), Quaternion.identity);
+                    blockMap[i, j] = newBlock;
+                }
             }
         }
     }
+
 }
