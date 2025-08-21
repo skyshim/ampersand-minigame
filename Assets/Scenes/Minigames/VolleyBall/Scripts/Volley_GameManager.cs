369d5051e7d974329347e0ce0fd7e4a934381f2b
skyshim <skyshim0128@gmail.com>
2025-08-22 01:22:46 +0900

    2025.08.22 - VolleyBall(2)
    시스템 완성, 디자인 + 효과음 필요

--- Assets/Scenes/Minigames/VolleyBall/Scripts/Volley_GameManager.cs
+++ Assets/Scenes/Minigames/VolleyBall/Scripts/Volley_GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Volley_GameManager : MonoBehaviour
 {
@@ -13,12 +15,25 @@ public class Volley_GameManager : MonoBehaviour
     public TMP_Text p2ScoreText;
     public GameObject ball;
 
+    public GameObject countdownPanel;
+    public TMP_Text countdownText;
+
+    public GameObject gameOverPanel;
+    public TMP_Text winnerText;
+    public Button retryButton;
+    public Button homeButton;
+
     public int player1Score = 0;
     public int player2Score = 0;
 
     void Start()
     {
+        retryButton.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+        homeButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu")); // 메인 메뉴 씬 이름 확인
+
+        gameOverPanel.SetActive(false);
         UpdateScoreUI();
+        StartCoroutine(GameStartCountdown());
     }
 
     // Ball에서 호출
@@ -40,27 +55,83 @@ public class Volley_GameManager : MonoBehaviour
         }
 
         UpdateScoreUI();
-        ResetRound(player1Serves);
+        StartCoroutine(EndRound(player1Serves));
+    }
+    void GameOver(int winner)
+    {
+        gameOverPanel.SetActive(true);
+        if (winner == 1) winnerText.text = "파란팀 승리!";
+        else if (winner == 2) winnerText.text = "빨간팀 승리!";
     }
 
     // 라운드 초기화
-    void ResetRound(bool player1Serves)
+    IEnumerator EndRound(bool player1Serves)
     {
         Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Static;
 
-        // 공 서브 위치 지정
+        // 1초 대기
+        yield return new WaitForSeconds(1f);
+        if (player1Score >= 5)
+        {
+            GameOver(1);
+        }
+        else if (player2Score >= 5)
+        {
+            GameOver(2);
+        }
+        else
+        {
+            StartRound(player1Serves);
+        }
+        // 다음 라운드 시작
+
+        
+    }
+
+    IEnumerator GameStartCountdown()
+    {
+        // 카운트다운 Panel 켜기
+        countdownPanel.SetActive(true);
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+
+        // 카운트다운 (3,2,1,시작!)
+        for (int i = 3; i > 0; i--)
+        {
+            countdownText.text = i.ToString();
+            yield return new WaitForSeconds(0.7f);
+        }
+        countdownText.text = "시작!";
+        yield return new WaitForSeconds(0.5f);
+
+        // Panel 끄기
+        countdownPanel.SetActive(false);
+        rb.isKinematic = false;
+
+        // 첫 라운드 시작 (Player1이 서브한다고 가정)
+        StartRound(true);
+    }
+
+    public void StartRound(bool player1Serves)
+    {
+        // 플레이어 위치 초기화
+        GameObject p1 = GameObject.Find("Player1");
+        GameObject p2 = GameObject.Find("Player2");
+        if (p1 != null) p1.transform.position = player1Spawn.position;
+        if (p2 != null) p2.transform.position = player2Spawn.position;
+
+        // 공 리셋
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        
+
         if (player1Serves)
             ball.transform.position = ball1Spawn.position;
         else
             ball.transform.position = ball2Spawn.position;
-
-        // 플레이어 위치 초기화
-        GameObject p1 = GameObject.Find("Player1");
-        GameObject p2 = GameObject.Find("Player2");
-
-        if (p1 != null) p1.transform.position = player1Spawn.position;
-        if (p2 != null) p2.transform.position = player2Spawn.position;
     }
 
     // UI 점수 갱신
