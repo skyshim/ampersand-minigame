e1bf46c12f7765815ac1a46b83cd700b6875afcc
skyshim <skyshim0128@gmail.com>
2025-08-29 22:37:47 +0900

    안드로이드 빌드 시도
    개같이멸망

--- Assets/Scenes/Minigames/DinoJump/DinoScript.cs
+++ Assets/Scenes/Minigames/DinoJump/DinoScript.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using TMPro;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class DinoScript : MonoBehaviour
