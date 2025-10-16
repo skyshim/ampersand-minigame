55f9498071688e5a55fb5f25fb615d1fe2742282
신범서 <sapltv090410@gmail.com>
2025-10-17 00:52:27 +0900

    공룡게임 장애물 적용중

--- Assets/Scenes/Minigames/DinoJump/ThingScript.cs
+++ Assets/Scenes/Minigames/DinoJump/ThingScript.cs
@@ -7,6 +7,9 @@ public class ThingScript : MonoBehaviour
     public GameObject manager;
     public DinoJumpManagerScript managerScript;
     public Rigidbody2D rb;
+    public Sprite[] obstacleSprites;
+    private SpriteRenderer sr;
+
 
     public int thingType;
     public float x, y;
@@ -16,10 +19,41 @@ public class ThingScript : MonoBehaviour
     {
         manager = GameObject.Find("Main Camera&DinoJump Manager");
         managerScript = manager.GetComponent<DinoJumpManagerScript>();
-        x = Random.Range(1f, 2.3f);
-        y = Random.Range(1.5f, 2f);
+        x = Random.Range(1f, 1.8f);
+        y = Random.Range(0.5f, 1.8f);
         rb = GetComponent<Rigidbody2D>();
-        transform.localScale = new Vector3(y, x, 1);
+        sr = GetComponent<SpriteRenderer>();
+
+        transform.localScale = new Vector3(x, y, 1);
+        transform.rotation = Quaternion.identity;
+        sr.color = Color.white;
+
+        if (x <= 1.3f)
+        {
+            if (y <= 1f) sr.sprite = obstacleSprites[3];
+            else if (y <= 1.5) sr.sprite = obstacleSprites[0];
+            else sr.sprite = obstacleSprites[4];
+        }
+        else if (x <= 1.6f)
+        {
+            if (y <= 1.6f) sr.sprite = obstacleSprites[0];
+            else sr.sprite = obstacleSprites[1];
+        }
+        else
+        {
+            if (y <= 1.5f) sr.sprite = obstacleSprites[2];
+            else sr.sprite = obstacleSprites[1];
+        }
+        PolygonCollider2D pc = GetComponent<PolygonCollider2D>();
+        Sprite currentSprite = sr.sprite;
+
+        pc.pathCount = currentSprite.GetPhysicsShapeCount();
+        for (int i = 0; i < pc.pathCount; i++)
+        {
+            var path = new List<Vector2>();
+            currentSprite.GetPhysicsShape(i, path);
+            pc.SetPath(i, path.ToArray());
+        }
     }
 
 
